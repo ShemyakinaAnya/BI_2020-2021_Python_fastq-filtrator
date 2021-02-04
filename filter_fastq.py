a4a4b0c0b6a0669795c555e9b139f2334375fe4d
KristinaGainova <gaynovakristina@gmail.com>
2021-02-04 11:53:26 +0300

    fixed error of creation empty file with failed reads

--- filter_fastq.py
+++ filter_fastq.py
@@ -41,7 +41,7 @@ if output_base_name is None:
 
 # fastq_filter programm
 def check_read_length(min_length, seq_read):
-    return True if min_length is None or len(seq_read) >= min_length else False
+    return min_length is None or len(seq_read) >= min_length
 
 
 def check_read_gc(gc_bound_min, gc_bound_max, seq_read):
@@ -62,20 +62,24 @@ def check_read_gc(gc_bound_min, gc_bound_max, seq_read):
 output_file_passed_name = output_base_name + '__passed.fastq'
 output_file_failed_name = output_base_name + '__failed.fastq'
 
-with open(input_file) as fastq_file, open(output_file_passed_name, 'w') as p, open(output_file_failed_name, 'w') as f:
+with open(input_file) as fastq_file, open(output_file_passed_name, 'w') as p:
+    if keep_filtered:
+        f = open(output_file_failed_name, 'w')
+
+    string_1 = fastq_file.readline()
+    while string_1:
+        string_2 = fastq_file.readline()
+        string_3 = fastq_file.readline()
+        string_4 = fastq_file.readline()
+        read = string_1 + string_2 + string_3 + string_4
+
+        read_is_good = check_read_length(min_length, string_2) and check_read_gc(gc_bounds[0], gc_bounds[1], string_2)
+        if read_is_good:
+            p.write(read)
+        else:
+            if keep_filtered:
+                f.write(read)
 
         string_1 = fastq_file.readline()
-        while string_1:
-            string_2 = fastq_file.readline()
-            string_3 = fastq_file.readline()
-            string_4 = fastq_file.readline()
-            read = string_1 + string_2 + string_3 + string_4
-
-            read_is_good = check_read_length(min_length, string_2) and check_read_gc(gc_bounds[0], gc_bounds[1], string_2)
-            if read_is_good:
-                p.write(read)
-            else:
-                if keep_filtered:
-                    f.write(read)
-
-            string_1 = fastq_file.readline()
+    if keep_filtered:
+        f.close()
